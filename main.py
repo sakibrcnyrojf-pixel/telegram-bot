e1af399c128536edf0d0dec3ae4467b0d82317da
sakibrcnyrojf-pixel <sakibrcnyrojf@gmail.com>
2026-09-08 13:02:50 +0600

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def is_user_joined(bot, user_id):
     return False
 
 # ----------------- ৪. বট কমান্ড হ্যান্ডলার -----------------
-BOT_TOKEN = "8697610230:AAFzzjFmO_VzOeC48vRf51uRkQY14rU14uU"
+BOT_TOKEN = "8697610230:AAFUChryG7XjOt_Cu25tYXmWCKY0_colmq8"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
